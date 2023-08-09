369be7a37b888e0472ac7f68ee9cdb041d4b8984
deshapriya <deshapriyakhatua@gmail.com>
2023-08-09 13:26:11 +0530

    dslj

--- contextmenus/contentScript.js
+++ contextmenus/contentScript.js
@@ -1,114 +1,81 @@
-let section = `<section id="containerr">
-    <div id="toolbar">
-        <label for="stroke">Stroke</label>
-        <input id="stroke" name='stroke' type="color">
-        <label for="lineWidth">Line Width</label>
-        <input id="lineWidth" name='lineWidth' type="number" value="5">
-        <button id="clear">Clear</button>
-    </div>
-    <div class="drawing-board">
-        <canvas id="drawing-board"></canvas>
-    </div>
-</section>`;
-document.body.innerHTML = section + document.body.innerHTML;
-const style  = document.createElement("style");
-style.innerHTML = `#containerr {
-                        height: 100%;
-                        display: flex;
-                       /* position: fixed; */
-                        left:0px;
-                        top:0px;
-                        z-index: 10099;
-                        border: 1px solid red;
-                    }
+var sectionTag = document.createElement("section");
+sectionTag.id = "containerr";
 
-                    #toolbar {
-                        display: flex;
-                        flex-direction: column;
-                        padding: 5px;
-                        width: 70px;
-                        background-color: #202020;
-                    }
+var canvasTag = document.createElement("canvas");
+canvasTag.id = "drawing-board";
 
-                    #toolbar * {
-                        margin-bottom: 6px;
-                    }
+sectionTag.appendChild(canvasTag);
+document.body.prepend(sectionTag);
 
-                    #toolbar label {
-                        font-size: 12px;
-                    }
-
-                    #toolbar input {
-                        width: 100%;
-                    }
-
-                    #toolbar button {
-                        background-color: #1565c0;
-                        border: none;
-                        border-radius: 4px;
-                        color:white;
-                        padding: 2px;
-                    }`;
+const style = document.createElement("style");
+style.innerHTML = `#drawing-board {
+                    position: absolute;
+                    border: 1px solid red;
+                    z-index: 1000000;
+                }`;
 document.head.append(style);
 
 const canvas = document.getElementById('drawing-board');
-const toolbar = document.getElementById('toolbar');
 const ctx = canvas.getContext('2d');
 
-const canvasOffsetX = canvas.offsetLeft;
-const canvasOffsetY = canvas.offsetTop;
-
-canvas.width = window.innerWidth - canvasOffsetX;
-canvas.height = window.innerHeight - canvasOffsetY;
+canvas.width = window.innerWidth;
+canvas.height = document.body.scrollHeight;
 
 let isPainting = false;
-let lineWidth = 5;
-let startX;
-let startY;
+let dragInputLineWidth = 5;
 
-toolbar.addEventListener('click', e => {
-    if (e.target.id === 'clear') {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        //document.body.style.overflow = "auto";
-    }
-});
+ctx.strokeStyle = "blue";
 
-toolbar.addEventListener('change', e => {
-    if(e.target.id === 'stroke') {
-        ctx.strokeStyle = e.target.value;
-    }
 
-    if(e.target.id === 'lineWidth') {
-        lineWidth = e.target.value;
-    }
-    
-});
+const startDraw = (e) => {
 
-// drawing 
+    isPainting = true;
+    ctx.beginPath();
+    ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop + window.pageYOffset);
+    e.preventDefault();
+
+};
+
 const draw = (e) => {
-    if(!isPainting) {
-        return;
-    }
-    
-    //document.body.style.overflow="hidden";
 
-    ctx.lineWidth = lineWidth;
+    if (!isPainting) return;
+
+    ctx.lineWidth = dragInputLineWidth;
     ctx.lineCap = 'round';
-
-    ctx.lineTo(e.clientX - canvasOffsetX, e.clientY);
+    ctx.lineJoin = "round";
+    ctx.strokeStyle = "blue";
+    ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop + window.pageYOffset);
     ctx.stroke();
+    e.preventDefault();
+    console.log(e.clientY);
 }
 
-canvas.addEventListener('mousedown', (e) => {
-    isPainting = true;
-    startX = e.clientX;
-    startY = e.clientY;
-});
+const endDraw = (e) => {
 
-canvas.addEventListener('mouseup', e => {
+    if (!isPainting) return;
+
     isPainting = false;
     ctx.stroke();
-    ctx.beginPath();
-});
+    ctx.closePath();
+    e.preventDefault();
 
+}
+
+const clear = (e) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+canvas.addEventListener('mousedown', startDraw);
 canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', endDraw);
+canvas.addEventListener('mouseout', endDraw);
+
+canvas.addEventListener('touchstart', startDraw);
+canvas.addEventListener('touchmove', draw);
+canvas.addEventListener('touchend', endDraw);
+
+window.addEventListener("scroll", () => {
+    //canvas.height = window.innerHeight + window.pageYOffset;
+    console.log(document.body.scrollHeight);
+    console.log(window.pageYOffset + window.innerHeight);
+})
